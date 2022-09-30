ceaec61fe466a043c9e7414d4864e53882949770
Dany Cervantes <danycervantesq@gmail.com>
2022-09-30 01:52:16 -0400

    Update robot.js
    Added Hal 9000 voice on wallet click.
    For Fun :)

--- robot.js
+++ robot.js
@@ -1,26 +1,38 @@
-function printer() {
+var hal9000 = document.querySelector(".wallet");
+
+//makes wallet numbers count upwards
+function crypto() {
     var print;
     var mint = document.getElementById("mint");
     var i = 0;
     
     function brr(){
-
         i += 1;
         mint.innerHTML = i;
 
         if (i >= 4444) {
             clearInterval(print);
             i = 0;
-            setTimeout(function() { printer() }, 10000);
+            setTimeout(function() { crypto() }, 10000);
         }
     }
-
     print = setInterval(brr, 100);
 }
 
 
+//plays sound on wallet click
+hal9000.addEventListener("click", function() {  
+    
+    hal();
+
+    function hal() {
+        var sorry = new Audio('assets/sorry.mp3');
+        sorry.play();
+    }
+});
+
 window.onload = function() {
     
-    printer();
+    crypto();
 
 }
